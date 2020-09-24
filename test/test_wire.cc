dc295b4940805bd59212ff8e07ea81571d18c733
Saba Sehirsh <ssehrish@fnal.gov>
2020-09-23 22:43:32 -0500

    working serialization of wire and test

---
+++ test/test_wire.cc
@@ -0,0 +1,47 @@
+#include "catch.hpp"
+#include "../wire_serialization.h"
+#include "larcoreobj/SimpleTypesAndConstants/RawTypes.h"
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/version.hpp>
+
+#include <fstream>
+
+TEST_CASE("Wire comparison works")
+{
+  std::vector<float> tmp{2.2, 4.3, 6.5, 8.7, 9.0};
+  raw::ChannelID_t channel = 56;
+  geo::View_t view = geo::k3D;  
+  lar::sparse_vector<float> sv(tmp, 0);
+  recob::Wire wire(sv, channel, view);
+  std::vector<float> tmp1{2.2, 4.3, 6.6, 8.7, 9.0};
+  lar::sparse_vector<float> sv1(tmp1, 0);
+  recob::Wire wire1(sv1, channel, view);
+  CHECK(!(wire1 == wire)); // to check wires constructed from unequal sparse_vectors
+}
+
+TEST_CASE("writing a wire works")
+{
+  std::vector<float> tmp{2.2, 4.3, 6.5, 8.7, 9.0};
+  raw::ChannelID_t channel = 56;
+  geo::View_t view = geo::k3D;  
+  lar::sparse_vector<float> sv(tmp, 0);
+  recob::Wire wire(sv, channel, view);
+  {
+    std::ofstream ofs("wireout");
+    boost::archive::binary_oarchive oa(ofs);
+    oa << wire;
+    CHECK(ofs.good());
+    ofs.close();
+  }
+  std::ifstream ifs("wireout");
+  CHECK(ifs.good());
+  boost::archive::binary_iarchive ia(ifs);
+  recob::Wire w; 
+  CHECK(!(w == wire));
+  ia >> w;
+  CHECK( wire == w );
+}
--- wire_serialization.h
+++ wire_serialization.h
@@ -8,14 +8,55 @@
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/version.hpp>
 
+namespace recob {
+
+inline bool
+  operator==(const recob::Wire& aw, const recob::Wire& bw) {
+    if(aw.Channel() != bw.Channel()) return false;
+    if(aw.View() != bw.View()) return false;
+    auto const a = aw.SignalROI();
+    auto const b = bw.SignalROI();
+    if(a.size() != b.size()) return false;
+    for(auto i=0;i<a.size();++i)
+      if (a[i] != b[i]) return false;
+    return true;
+  }
+}
+
 namespace boost {
   namespace serialization {
     template <class Archive>
     void
+    save(Archive& ar, const lar::sparse_vector<float>& sv, const unsigned int) {
+      auto sz = sv.size();
+      ar << sz;
+      for (auto i=0;i<sz; ++i)
+        ar << sv[i];
+    }
+
+    template<class Archive>
+    void
+    load(Archive&ar, lar::sparse_vector<float>& sv, const unsigned int) {
+      size_t sz;
+      ar >> sz;
+      std::vector<float> tmp;
+      tmp.resize(sz);
+      for (auto i = 0 ; i<sz; ++i) {
+        ar >> tmp[i];
+      }
+      sv = lar::sparse_vector<float>(tmp, 0);
+      
+    }
+
+    template <class Archive>
+    void
     save(Archive& ar, const recob::Wire& wire, const unsigned int) {
-      ar << wire.Channel();
-      ar << wire.View();
-      ar <<  wire.SignalROI();
+      auto c = wire.Channel();
+      ar << c;
+      auto v =  wire.View();
+      ar <<  v;
+      auto roi = wire.SignalROI();
+      ar << roi;
     }
 
     template <class Archive>
@@ -23,11 +64,17 @@ namespace boost {
     load(Archive& ar, recob::Wire& wire, const unsigned int) {
       raw::ChannelID_t channel;
       geo::View_t view;
-      RegionsOfInterest_t rois;
-
-      recob::Wire w();
+      lar::sparse_vector<float> rois;
+      ar >> channel;
+      ar >> view;
+      std::cout << "read channel and view\n"; 
+      ar >> rois;
+      std::cout << "read rois\n"; 
+      recob::Wire w(rois, channel, view);
       wire = w;
     }
   
   }
 }
+BOOST_SERIALIZATION_SPLIT_FREE(lar::sparse_vector<float>);
+BOOST_SERIALIZATION_SPLIT_FREE(recob::Wire);
