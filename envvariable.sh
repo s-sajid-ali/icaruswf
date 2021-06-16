51e5d561aa8bf343beffa8df15c502d5212c498b
Marc Paterno <paterno@fnal.gov>
2021-06-16 17:23:38 -0500

    Progress on build of merge module

--- envvariable.sh
+++ envvariable.sh
@@ -1,5 +1,6 @@
-export SRC=/scratch/ssehrish/icaruswf
+#export SRC=/scratch/ssehrish/icaruswf
 
+export ART_DIR=`spack location -i art`
 export ARTDAQ_CORE_DIR=`spack location -i artdaq-core`
 export BOOST_DIR=`spack location -i boost`
 export CETLIB_EXCEPT_DIR=`spack location -i cetlib-except`
@@ -10,6 +11,7 @@ export LARCOREOBJ_DIR=`spack location -i larcoreobj`
 export ROOTSYS=`spack location -i root`
 
 
+export ART_INC=${ART_DIR}/include
 export ARTDAQ_CORE_INC=${ARTDAQ_CORE_DIR}/include
 export BOOST_INC=${BOOST_DIR}/include
 export CETLIB_EXCEPT_INC=${CETLIB_EXCEPT_DIR}/include
@@ -19,6 +21,7 @@ export LARDATAOBJ_INC=${LARDATAOBJ_DIR}/include
 export LARCOREOBJ_INC=${LARCOREOBJ_DIR}/include
 export ROOT_INC=${ROOTSYS}/include
 
+export ART_LIB=${ART_DIR}/lib
 export BOOST_LIB=${BOOST_DIR}/lib
 export ARTDAQ_CORE_LIB=${ARTDAQ_CORE_DIR}/lib
 export CETLIB_LIB=${CETLIB_DIR}/lib
