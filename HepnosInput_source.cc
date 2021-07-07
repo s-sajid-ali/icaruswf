1f773384c2b869a6f2d6fc8ba16ba092c06a8c91
Saba Sehrish <ssehrish@fnal.gov>
2021-07-07 11:11:52 -0500

    added loading assns from Hepnos store functionality

--- HepnosInput_source.cc
+++ HepnosInput_source.cc
@@ -1,3 +1,4 @@
+
 #include "art/Framework/Principal/Handle.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Core/SharedAnalyzer.h"
@@ -5,6 +6,7 @@
 #include "art/Framework/IO/Sources/Source.h"
 #include "art/Framework/Core/InputSourceMacros.h"
 #include "canvas/Persistency/Common/Assns.h"
+#include "canvas/Persistency/Provenance/canonicalProductName.h"
 
 #include "art/Framework/Core/ProductRegistryHelper.h"
 #include "art/Framework/IO/Sources/SourceHelper.h"
@@ -28,7 +30,9 @@
 #include "lardataobj/RecoBase/SpacePoint.h"
 #include "lardataobj/RecoBase/Wire.h"
 
+#define private public
 #include "hepnos.hpp"
+#undef private
 
 #include "hit_serialization.h"
 #include "rawdigit_serialization.h"
@@ -44,6 +48,53 @@ namespace hepnos {
 }
 
 namespace hepnossource{
+  template <typename T>
+  std::unique_ptr<std::vector<T>> 
+  loaddata(hepnos::Event& event, 
+           std::string const& module_label) 
+  {
+     art::InputTag const tag(module_label, "", "");
+     std::vector<T> products;
+     event.load(tag, products);
+     return std::make_unique<std::vector<T>>(products);
+  }
+  
+  template <typename T>
+  art::ProductID
+  create_productID(std::vector<T> const& t, 
+                   std::string const& module_label, 
+                   std::string const& instance_name="") 
+  {
+     auto const product_name = 
+             art::canonicalProductName( art::TypeID{t}.friendlyClassName(), 
+                                        module_label, 
+                                        instance_name, 
+                                        art::Globals::instance()->processName());
+     return art::ProductID{product_name};      
+  } 
+ 
+  template <typename A, typename B>
+  std::unique_ptr<art::Assns<A, B>>
+  loadassns(hepnos::Event& event, 
+            art::EventPrincipal*& outE, 
+            std::vector<A> const& a_col, 
+            std::vector<B> const& b_col, 
+            std::string const& a_module_label, 
+            std::string const& b_module_label)
+  {
+     art::InputTag const tag(a_module_label, "", "");
+     std::vector<std::pair<hepnos::Ptr<A>, hepnos::Ptr<B>>> assns;
+     event.load(tag, assns);
+     art::Assns<A, B> art_assns;
+     auto const a_pid = create_productID<A>(a_col, a_module_label, "");
+     auto const b_pid = create_productID<B>(b_col, b_module_label, "");
+     for (auto const& [a, b]: assns) {
+       art_assns.addSingle(art::Ptr<A>(a_pid, a.m_index, outE->productGetter(a_pid)), 
+                           art::Ptr<B>(b_pid, b.m_index, outE->productGetter(b_pid)));
+     }
+     return std::make_unique<art::Assns<A, B>>(art_assns);
+  }
+
   class HepnosInputSource {
   public:
     //struct Config {
@@ -51,9 +102,13 @@ namespace hepnossource{
   
     //using Parameters = fhicl::Table<Config>;
   
-    HepnosInputSource(fhicl::ParameterSet const& p, art::ProductRegistryHelper& rh, art::SourceHelper const& pm) : pm_(pm)
+    HepnosInputSource(fhicl::ParameterSet const& p, 
+                      art::ProductRegistryHelper& rh, 
+                      art::SourceHelper const& pm) : pm_(pm)
     {
       rh.reconstitutes<std::vector<recob::Hit>, art::InEvent>("gaushit");
+      rh.reconstitutes<std::vector<recob::Wire>, art::InEvent>("recowireraw");
+      rh.reconstitutes<art::Assns<recob::Hit, recob::Wire>, art::InEvent>("gaushit");
     }
 
     bool readNext(art::RunPrincipal* const& inR,
@@ -86,11 +141,10 @@ namespace hepnossource{
 
     bool
     HepnosInputSource::readNext(art::RunPrincipal* const& inR,
-                  art::SubRunPrincipal* const& inSR,
-                  art::RunPrincipal*& outR,
-                  art::SubRunPrincipal*& outSR,
-                  art::EventPrincipal*& outE)
- 
+                                art::SubRunPrincipal* const& inSR,
+                                art::RunPrincipal*& outR,
+                                art::SubRunPrincipal*& outSR,
+                                art::EventPrincipal*& outE) 
     {
       if (ev_ == es_->end()) return false;
       art::Timestamp ts;
@@ -108,23 +162,23 @@ namespace hepnossource{
         sr_ = sr;
       }
       outE = pm_.makeEventPrincipal(r, sr, ev_->number(), ts, false);
-      art::InputTag const tag("gaushit", "", "");
-      std::vector<recob::Hit> hits;
-      ev_->load(tag, hits);
-      std::cout << hits.size() << "\n";
-      std::unique_ptr<std::vector<recob::Hit>> v1 = std::make_unique<std::vector<recob::Hit>>(hits);
-      art::put_product_in_principal(std::move(v1), *outE, "gaushit");
+      
+      auto hits = loaddata<recob::Hit>(*ev_, "gaushit");
+      auto wires = loaddata<recob::Wire>(*ev_, "recowireraw");
+      auto assns = loadassns<recob::Hit, recob::Wire>(*ev_, outE, *hits, *wires, "gaushit", "recowireraw");
+      art::put_product_in_principal(std::move(hits), *outE, "gaushit");
+      art::put_product_in_principal(std::move(wires), *outE, "recowireraw");
+      art::put_product_in_principal(std::move(assns), *outE, "gaushit");
       ++ev_;
       return true;
     }
 }
+
 namespace art {
-  // We don't want the file services: we must say so by specializing the                                                                                                                                                                 
-  //   // template *before* specifying the typedef.                                                                                                                                                                                           
-template <>
+ // We don't want the file services: we must say so by specializing the             // template *before* specifying the typedef.
+  template <>
   struct Source_wantFileServices<hepnossource::HepnosInputSource> {
       static constexpr bool value{false};
-        };
-        } // namespace art
-//typedef art::Source<HepnosInputSource> HepnosInput;
+  };
+} // namespace art
 DEFINE_ART_INPUT_SOURCE(art::Source<hepnossource::HepnosInputSource>);
