2e1989f541ac4d66452b9e6697a3b3a67e51f01d
Saba Sehrish <ssehrish@fnal.gov>
2022-02-24 22:25:53 -0600

    added edge serialization and tests

---
+++ test/test_edge.cc
@@ -0,0 +1,39 @@
+#include "catch.hpp"
+#include "../edge_serialization.h"
+#include "larcoreobj/SimpleTypesAndConstants/RawTypes.h"
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/version.hpp>
+
+#include <fstream>
+
+TEST_CASE("Edge comparison works")
+{
+  recob::Edge e1(20., 3, 4, 5);
+  recob::Edge e2(20., 3, 4, 5);
+  recob::Edge e3(45., 3, 4, 5);
+  CHECK(e1==e2); 
+  CHECK(!(e1==e3)); 
+}
+
+TEST_CASE("writing an edge works")
+{
+  recob::Edge edge(20., 3, 4, 5);
+  {
+    std::ofstream ofs("edgeout");
+    boost::archive::binary_oarchive oa(ofs);
+    oa << edge;
+    CHECK(ofs.good());
+    ofs.close();
+  }
+  std::ifstream ifs("edgeout");
+  CHECK(ifs.good());
+  boost::archive::binary_iarchive ia(ifs);
+  recob::Edge e; 
+  CHECK(!(e == edge));
+  ia >> e;
+  CHECK( e == edge );
+}
