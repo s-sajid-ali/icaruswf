956790005306d740f64b7170d9fbff94e2eb78e3
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-03-04 14:57:13 -0600

    add a test for recob::Seed's == operator that fails

--- test/test_seed.cc
+++ test/test_seed.cc
@@ -30,6 +30,10 @@ TEST_CASE("writing a seed works")
   boost::archive::binary_iarchive ia(ifs);
   recob::Seed s2;
   ia >> s2;
-
   CHECK(s1 == s2);
+
+  std::for_each(std::begin(Pt), std::end(Pt), [](auto& x) { x += 1; });
+
+  recob::Seed s3(Pt.data(), Dir.data(), PtErr.data(), DirErr.data());
+  CHECK_FALSE(s1 == s3);
 }
