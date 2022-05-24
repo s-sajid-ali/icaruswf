537a49d39bb7ca0231e3f49d7a1fb4420380e95d
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-05-24 17:25:37 -0500

    update

--- scripts/csresearch00/run_icaruswf_hepnos_bench.sh
+++ scripts/csresearch00/run_icaruswf_hepnos_bench.sh
@@ -20,6 +20,14 @@ export DATA_DIR=/scratch/cerati/icaruscode-v09_37_01_02p02/icaruscode-09_37_01_0
 # for geometry files!
 export FW_SEARCH_PATH=${FW_SEARCH_PATH}:/scratch/gartung/spack/opt/spack/linux-scientific7-x86_64_v2/gcc-9.3.0/icarusalg-09.37.01-gz6rajahmifzikivvlrnufwz5z2hugn4/gdml
 
+# start the hepnos server
+mpirun -np 2 bedrock ofi+tcp -c ${ICARUSWF_BUILD}/test/hepnos.json &
+sleep 30
+hepnos-list-databases ofi+tcp -s hepnos.ssg > connection.json
+
+# Store data (raw::RawDigits) to hepnos for the first step, signal processing
+art -c storedata.fcl -s /scratch/cerati/icaruscode-v09_37_01_02p02/icaruscode-09_37_01_02p02-samples/prodcorsika_bnb_genie_protononly_overburden_icarus_20220118T213827-GenBNBbkgr_100evt_G4_DetSim.root -n 1
+
 export BASEDIR=$(pwd)
 # Create all the directories!
 for THREADS in 1
@@ -32,9 +40,13 @@ do
   for RUN in {1..5}
   do
     mkdir -p run_$RUN
+    cd run_$RUN
+    cp $BASEDIR/connection.json .
   done
 done
 cd $BASEDIR
+
+
 
 # run the benchmark
 for RUN in 1
@@ -42,21 +54,19 @@ do
   # run icaruswf-bench with varying number of client threads
   for THREADS in 1
   do
-      cd ${BASEDIR}/threads_$THREADS/run_$RUN
+      cd $BASEDIR/threads_$THREADS/run_$RUN
       export NUM_CLIENT_THREADS_PER_RANK=$THREADS
 
       echo "%%% before icaruswf-root-sigproc with $THREADS threads, run number $RUN at $(date)"
-      art --nschedules 1 --nthreads ${NUM_CLIENT_THREADS_PER_RANK} -c sp_root.fcl -s ${DATA_DIR}/prodcorsika_bnb_genie_protononly_overburden_icarus_20220118T213827-GenBNBbkgr_100evt_G4_DetSim.root &> sp_out
+      art --nschedules 1 --nthreads ${NUM_CLIENT_THREADS_PER_RANK} -c sp_hepnos.fcl &> sp_out
       echo "%%% after icaruswf-root-sigproc with $THREADS threads, run number $RUN at $(date)"
 
       echo "%%% before icaruswf-root-hit-find with $THREADS threads, run number $RUN at $(date)"
-      infile=$(find .  -name "prodcorsika*SignalProcessing\.root")
-      art --nschedules 1 --nthreads ${NUM_CLIENT_THREADS_PER_RANK} -c hf_root.fcl -s $infile &> hf_out
+      art --nschedules 1 --nthreads ${NUM_CLIENT_THREADS_PER_RANK} -c hf_hepnos.fcl &> hf_out
       echo "%%% after icaruswf-root-hit-find with $THREADS threads, run number $RUN at $(date)"
 
       echo "%%% before icaruswf-root-pandora with $THREADS threads, run number $RUN at $(date)"
-      infile=$(find .  -name "prodcorsika*HitFinding\.root")
-      art --nschedules 1 --nthreads ${NUM_CLIENT_THREADS_PER_RANK} -c p_root.fcl -s $infile &> p_out
+      art --nschedules 1 --nthreads ${NUM_CLIENT_THREADS_PER_RANK} -c p_hepnos.fcl &> p_out
       echo "%%% after icaruswf-root-pandora with $THREADS threads, run number $RUN at $(date)"
 
   done
