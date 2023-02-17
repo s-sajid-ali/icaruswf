ac5ed1c965154271cddec5fe50934b7b755d4e5b
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2023-02-17 11:10:40 -0600

    merge conflict fix
    	modified:   src/modules/mpi_wrapper.cc

--- src/modules/mpi_wrapper.cc
+++ src/modules/mpi_wrapper.cc
@@ -10,9 +10,12 @@
 #include <unistd.h>
 
 struct Config {
-  bool valid() const{
-   // This logic needs to be more robust, 
-   // the configuration is valid if either load path or process flag is specified not both. 
+  bool
+  valid() const
+  {
+    // This logic needs to be more robust,
+    // the configuration is valid if either load path or process flag is
+    // specified not both.
     return (process == loadPath.empty());
   };
   int64_t nevents{-1};
@@ -33,7 +36,7 @@ make_config(int argc, char* argv[])
     TCLAP::ValueArg<int64_t> nevtsArg(
       "n",
       "num_evts_per_rank",
-     "total number of events to process, by each MPI rank",
+      "total number of events to process, by each MPI rank",
       true,
       0,
       "int64");
@@ -78,7 +81,7 @@ make_config(int argc, char* argv[])
   return config;
 }
 
-// the fcl file for loading shouldn't be hard coded, 
+// the fcl file for loading shouldn't be hard coded,
 // for now its okay but should be fixed at some point
 
 int
@@ -107,7 +110,7 @@ loadData(int my_rank,
 }
 
 // this is to run the signal processing and hitfinding steps
-// as specified in the fcl file. the name is hard coded here 
+// as specified in the fcl file. the name is hard coded here
 // as well, may be we want to change it as well
 int
 runSH(int my_rank,
@@ -135,17 +138,17 @@ runSH(int my_rank,
   return std::system(cmd.c_str());
 }
 
-//retunr hostname along with 
-//printout of rank and total ranks
-//the printout is only relevant for debugging
+// retunr hostname along with
+// printout of rank and total ranks
+// the printout is only relevant for debugging
 std::string
-get_hostname(int my_rank, 
-               int nranks)
+get_hostname(int my_rank, int nranks)
 {
   constexpr size_t LEN = 1024;
   char host[LEN];
   if (gethostname(host, LEN) == 0) {
-    std::cout << "MPI rank " << my_rank << " of total " << nranks << " ranks is running on " << host << '\n';
+    std::cout << "MPI rank " << my_rank << " of total " << nranks
+              << " ranks is running on " << host << '\n';
   } else {
     std::cout << "could not determine execution resources being used!\n";
   }
@@ -162,19 +165,20 @@ main(int argc, char* argv[])
   if (!config.valid()) {
     std::cerr << "invalid configuration\n";
     return 1;
-  }  
+  }
 
   int64_t events = config.nevents;
   std::string root_file_path = config.loadPath;
   bool load_events_to_hepnos = !root_file_path.empty();
   bool process_events_in_hepnos = config.process;
   if (!process_events_in_hepnos && root_file_path.empty()) {
-      std::cerr << "please provide the path to the root file to load events from!\n";
-      return 1;
+    std::cerr
+      << "please provide the path to the root file to load events from! \n";
+    return 1;
   }
   int64_t nthreads = config.nthreads;
 
-  //initialize MPI after the configuration is all done and checked
+  // initialize MPI after the configuration is all done and checked
   Mpi world(argc, argv);
   auto my_rank = world.rank();
   auto nranks = world.np();
@@ -183,13 +187,13 @@ main(int argc, char* argv[])
   // nranks and my_rank are only used to print out the rank information
   // they are not needed for getting the hostname
   auto hostname = get_hostname(nranks, my_rank);
-  
+
   // Set special flags for running on Theta
-  std::optional<std::string> env_flags;   
+  std::optional<std::string> env_flags;
   if (hostname.find("theta") != std::string::npos) {
     env_flags.emplace("PMI_NO_PREINITIALIZE=1 PMI_NO_FORK=1");
   }
-  
+
   // finally either load events
   if (load_events_to_hepnos) {
     return loadData(my_rank, events, root_file_path, env_flags);
