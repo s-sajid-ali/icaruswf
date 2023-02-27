1f3ac02483735b805398657bc86dbf6cd53099db
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2023-02-27 12:29:47 -0600

    ROOT workflow wrapper

--- src/modules/root_mpi_wrapper.cc
+++ src/modules/root_mpi_wrapper.cc
@@ -1,5 +1,7 @@
 #include "mpicpp.hpp"
+#include <cstddef>
 #include <cstdlib>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <optional>
@@ -10,18 +12,9 @@
 #include <unistd.h>
 
 struct Config {
-  bool
-  valid() const
-  {
-    // This logic needs to be more robust,
-    // the configuration is valid if either load path or process flag is
-    // specified not both.
-    return (process == loadPath.empty());
-  };
   int64_t nevents{-1};
   int64_t nthreads{-1};
   std::string loadPath;
-  bool process{false};
 };
 
 Config
@@ -31,7 +24,7 @@ make_config(int argc, char* argv[])
   Config config;
   try {
     TCLAP::CmdLine cmd(
-      "ICARUSWF with HEPnOS mpi-wrapper for launching art over MPI!");
+      "ICARUSWF with ROOT mpi-wrapper for launching art over MPI!");
 
     TCLAP::ValueArg<int64_t> nevtsArg(
       "n",
@@ -43,7 +36,7 @@ make_config(int argc, char* argv[])
     cmd.add(nevtsArg);
 
     TCLAP::ValueArg<std::string> rootfilepathArg(
-      "l",
+      "f",
       "root_file_path",
       "location of the ROOT file to process",
       false,
@@ -60,12 +53,6 @@ make_config(int argc, char* argv[])
       "int64");
     cmd.add(threadsArg);
 
-    TCLAP::SwitchArg processSwitch(
-      "p",
-      "process_events",
-      "Whether to process events in the HEPnOS datastore",
-      false);
-    cmd.add(processSwitch);
     // Parse the argv array.
     cmd.parse(argc, argv);
     config.nevents = nevtsArg.getValue();
@@ -85,55 +72,55 @@ make_config(int argc, char* argv[])
 // for now its okay but should be fixed at some point
 
 int
-loadData(int my_rank,
-         int nevents,
-         std::string path,
-         std::optional<std::string> env_flags)
-{
-  std::string nskip = " --nskip ";
-  nskip += std::to_string(my_rank * nevents);
-  std::string nevts = " -n ";
-  nevts += std::to_string(nevents);
-  std::string cmd = "";
-  if (env_flags.has_value()) {
-    cmd.append(env_flags.value());
-  }
-  cmd.append("art -c storedata_queue.fcl -s ");
-  cmd.append(path);
-  cmd.append(nevts);
-  cmd.append(nskip);
-  std::string outfile = " &> ";
-  outfile.append(std::to_string(my_rank));
-  outfile.append("_load.txt");
-  cmd.append(outfile);
-  return std::system(cmd.c_str());
-}
-
-// this is to run the signal processing and hitfinding steps
-// as specified in the fcl file. the name is hard coded here
-// as well, may be we want to change it as well
-int
-runSH(int my_rank,
+runSP(int my_rank,
       int nevents,
-      int nthreads,
+      std::string path,
       std::optional<std::string> env_flags)
 {
   std::string nevts = " -n ";
   nevts += std::to_string(nevents);
   std::string memdb =
     " --memcheck-db memory_sp_" + std::to_string(my_rank) + ".db";
+  std::string timedb =
+    " --timing-db timing_sp_" + std::to_string(my_rank) + ".db";
 
   std::string cmd = "";
   if (env_flags.has_value()) {
     cmd.append(env_flags.value());
   }
   cmd.append("art --nschedules 1 --nthreads " + std::to_string(nthreads) +
-             " -c icarus_SH_queue.fcl "
-             "--timing-db timing_sp_" +
-             std::to_string(my_rank) + ".db" + memdb + nevts);
+             " -c sp_root.fcl " + timedb + memdb + nevts + " -s ");
+  cmd.append(path);
   std::string outfile = " &> ";
   outfile.append(std::to_string(my_rank));
-  outfile.append("_process.txt");
+  outfile.append("_sp.txt");
+  cmd.append(outfile);
+  return std::system(cmd.c_str());
+}
+
+int
+runHF(int my_rank,
+      int nevents,
+      std::string path,
+      std::optional<std::string> env_flags)
+{
+  std::string nevts = " -n ";
+  nevts += std::to_string(nevents);
+  std::string memdb =
+    " --memcheck-db memory_hf_" + std::to_string(my_rank) + ".db";
+  std::string timedb =
+    " --timing-db timing_hf_" + std::to_string(my_rank) + ".db";
+
+  std::string cmd = "";
+  if (env_flags.has_value()) {
+    cmd.append(env_flags.value());
+  }
+  cmd.append("art --nschedules 1 --nthreads " + std::to_string(nthreads) +
+             " -c hf_root.fcl " + timedb + memdb + nevts +
+             " -s sp_output.root");
+  std::string outfile = " &> ";
+  outfile.append(std::to_string(my_rank));
+  outfile.append("_sp.txt");
   cmd.append(outfile);
   return std::system(cmd.c_str());
 }
@@ -145,14 +132,14 @@ std::string
 get_hostname(int my_rank, int nranks)
 {
   constexpr size_t LEN = 1024;
-  char host[LEN];
-  if (gethostname(host, LEN) == 0) {
+  std::string hostname;
+  hostname.resize(LEN);
+  if (gethostname(hostname.data(), LEN) == 0) {
     std::cout << "MPI rank " << my_rank << " of total " << nranks
               << " ranks is running on " << host << '\n';
   } else {
     std::cout << "could not determine execution resources being used!\n";
   }
-  std::string hostname{host};
   return hostname;
 }
 
@@ -160,18 +147,10 @@ int
 main(int argc, char* argv[])
 {
   Config config = make_config(argc, argv);
-  // either we will configure this to run the data loading function or event
-  // processing For data loading
-  if (!config.valid()) {
-    std::cerr << "invalid configuration\n";
-    return 1;
-  }
 
   int64_t events = config.nevents;
   std::string root_file_path = config.loadPath;
-  bool load_events_to_hepnos = !root_file_path.empty();
-  bool process_events_in_hepnos = config.process;
-  if (!process_events_in_hepnos && root_file_path.empty()) {
+  if (root_file_path.empty()) {
     std::cerr
       << "please provide the path to the root file to load events from!\n";
     return 1;
@@ -190,16 +169,22 @@ main(int argc, char* argv[])
 
   // Set special flags for running on Theta
   std::optional<std::string> env_flags;
-  if (hostname.find("theta") != std::string::npos) {
-    env_flags.emplace("PMI_NO_PREINITIALIZE=1 PMI_NO_FORK=1");
+  // hostname on theta is just nid0000, so check that we are
+  // not running on csresearch for now.
+  if (hostname.find("csresearch") == std::string::npos) {
+    env_flags.emplace("PMI_NO_PREINITIALIZE=1 PMI_NO_FORK=1 ");
   }
 
-  // finally either load events
-  if (load_events_to_hepnos) {
-    return loadData(my_rank, events, root_file_path, env_flags);
+  // create a directory for this MPI rank and cd into it!
+  std::filesystem::create_directory(std::to_string(my_rank));
+  std::filesystem::current_path(std::to_string(my_rank));
+
+  if (runSP(my_rank, events, root_file_path, env_flags) != 0) {
+    std::cerr << "error when running signal processing with ROOT!";
   }
-  // OR process events
-  if (process_events_in_hepnos) {
-    return runSH(my_rank, events, nthreads, env_flags);
+  if (runHF(my_rank, events, root_file_path, env_flags) != 0) {
+    std::cerr << "error when running hit finding with ROOT!";
   }
+
+  return;
 }
